46379a1056d5d7abfb4edaf85d9d8d9a41c4f8d8
Ayan Chowdhury <ayan10092018@gmail.com>
2025-04-26 08:30:14 +0530

    Signin api route created

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,7 +1,8 @@
 import express from 'express';
 const router = express.Router();
-import { signup } from '../controller/auth.controller.js';
+import { signup, signin } from '../controller/auth.controller.js';
 
 router.post('/signup',signup);
+router.post('/login', signin);
 
 export default router;
