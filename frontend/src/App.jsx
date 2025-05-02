168a60eee6f59ef5e600673f61006dac479f28e2
Ayan Chowdhury <ayan10092018@gmail.com>
2025-05-03 00:53:59 +0530

    Update header and make profile page protected

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Signin from './pages/Signin';
 import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
 
 export default function App() {
   return <BrowserRouter>
@@ -15,7 +16,9 @@ export default function App() {
       <Route path="/about" element={<About />} />
       <Route path="/signin" element={<Signin />} />
       <Route path="/signup" element={<SignUp />} />
+      <Route element={<PrivateRoute />}>
       <Route path="/profile" element={<Profile />} />
+      </Route>
     </Routes>
   </BrowserRouter>;
 }
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import {FaSearch} from 'react-icons/fa';
 import {Link} from 'react-router-dom';
+import {useSelector} from 'react-redux';
 
 export default function Header() {
+  const {currentUser} = useSelector((state) => state.user);
   return (
     <header className='bg-amber-400 shadow-md'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -22,8 +24,11 @@ export default function Header() {
                 <Link to='/about'>
                     <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
                 </Link>
-                <Link to='/signin'>
-                <li className='text-slate-700 hover:underline'>Sign in</li>
+                <Link to='/profile'>
+                {currentUser ? (
+                    <img src={currentUser.avatar} className='w-7 h-7 rounded-full object-cover' />
+                 ) : ( <li className='text-slate-700 hover:underline'>Sign in</li>
+                    )}
                 </Link>
             </ul>
         </div>
